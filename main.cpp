8f88e55c699cf1920e317a13a6af38d1f8923a3d
Elijah Davis <nashisback96@gmail.com>
2016-04-25 11:16:21 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -285,21 +285,25 @@ void charMovement( Game *game)
 	if (p->s.center.y - p->s.height == 0 && p->velocity.y < 0) {
 		p->s.center.y = p->s.height;
 		p->velocity.y = 0;
+		shiftScreen(game, 'd');
 	}
 	//roof
 	if (p->s.center.y + p->s.height == WINDOW_HEIGHT && p->velocity.y > 0) {
 		p->s.center.y = WINDOW_HEIGHT - p->s.height;
 		p->velocity.y = 0;
+		shiftScreen(game, 'u');
 	}
 	//left wall
 	if (p->s.center.x - p->s.width <= 0 && p->velocity.x < 0) {
 		p->s.center.x = p->s.width;
 		p->velocity.x = 0;
+		shiftScreen(game, 'l');
 	}
 	//right wall
 	if (p->s.center.x + p->s.width >= WINDOW_WIDTH && p->velocity.x > 0) {
 		p->s.center.x = WINDOW_WIDTH - p->s.width;
 		p->velocity.x = 0;
+		shiftScreen(game, 'r');
 	}
 	
 	
