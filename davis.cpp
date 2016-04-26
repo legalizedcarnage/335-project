d809270e30a8a05a29e6c436988508616ef49901
Elijah Davis <nashisback96@gmail.com>
2016-04-26 08:13:24 -0700

    Update davis.cpp

--- davis.cpp
+++ davis.cpp
@@ -3,19 +3,26 @@
 using namespace std;
 void shiftScreen(Game *game, char direction)
 {
-    if (direction == 'u') {
-	game->map[1]++;
-    }
-    if (direction == 'd') {
-	game->map[1]--;
-    }
-    if (direction == 'l') {
-	game->map[0]--;
-    }
-    if (direction == 'r') {
-	game->map[0]++;
-    }
-    std::cout << game->map[0]  << ", " << game->map[1] << endl;
+	if (direction == 'u') {
+    		game->map[1]++;
+	}
+	if (direction == 'd') {
+		game->map[1]--;
+	}
+	if (direction == 'l') {
+		game->map[0]--;
+	}
+	if (direction == 'r') {
+    		game->map[0]++;
+	}
+	std::cout << game->map[0]  << ", " << game->map[1] << endl;
 
 }
-    
+void collision(Game *game)
+{
+	Shape *s;
+	for (int i; i < game->n; i++) {
+		s = &game->objects[i];
+	}
+		
+}
