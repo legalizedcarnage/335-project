ca8f7c016bf342bb7ec9f4453346cb7f93125b10
Miguel Trigueros <mtrigueros1@csub.edu>
2016-04-28 12:15:15 -0700

    Added files via upload

--- miguelT.cpp
+++ miguelT.cpp
@@ -32,70 +32,62 @@ void enemiesMovement(Game *game)
 	p->s.center.y = p->s.height + 20;
 	p->velocity.y *= -1;
 	ycount++;
-	if(ycount == 2) {
-	    if(p->velocity.y == directiony) {
-		int randx = rand() % 10;
-		cout << "randx: " << randx << endl;
-		p->velocity.x = 0;
-		p->velocity.y = 0;
-		if(randx <= 5)
-		    p->velocity.x = 8;
-		if(randx > 5)
-		    p->velocity.x = -8;
-		ycount = 0;
-	    }
+	if(ycount >= 2) {
+	    int randx = rand() % 10;
+	    cout << "randx: " << randx << endl;
+	    p->velocity.x = 0;
+	    p->velocity.y = 0;
+	    if(randx <= 5)
+		p->velocity.x = 8;
+	    if(randx > 5)
+		p->velocity.x = -8;
+	    ycount = 0;
 	}
     }
     if (p->s.center.y + p->s.height >= WINDOW_HEIGHT-20 && p->velocity.y > 0) {
 	p->s.center.y = WINDOW_HEIGHT - p->s.height - 20;
 	p->velocity.y *= -1;
 	ycount++;
-	if(ycount == 2) {
-	    if(p->velocity.y == directiony) {	
-		int randx = rand() % 10;
-		cout << "randx: " << randx << endl;
-		p->velocity.x = 0;
-		p->velocity.y = 0;
-		if(randx <= 5)
-		    p->velocity.x = 8;
-		if(randx > 5)
-		    p->velocity.x = -8;
-		ycount = 0;
-	    }
+	if(ycount >= 2) {
+	    int randx = rand() % 10;
+	    cout << "randx: " << randx << endl;
+	    p->velocity.x = 0;
+	    p->velocity.y = 0;
+	    if(randx <= 5)
+		p->velocity.x = 8;
+	    if(randx > 5)
+		p->velocity.x = -8;
+	    ycount = 0;
 	}
     }
     if (p->s.center.x - p->s.width <= 20 && p->velocity.x < 0) {
 	p->s.center.x = p->s.width + 20;
 	p->velocity.x *= -1;
 	xcount++;
-	if(xcount == 2) {
-	    if(p->velocity.x == directionx) {
-		int randy = rand() % 10;
-		cout << "randy: " << randy << endl;
-		p->velocity.x = 0;
-		p->velocity.x = 0;
-		if(randy <= 5)
-		    p->velocity.y = 8;
-		if(randy > 5)
-		    p->velocity.y = -8;
-		xcount = 0;
-	    }
+	if(xcount >= 2) {
+	    int randy = rand() % 10;
+	    cout << "randy: " << randy << endl;
+	    p->velocity.x = 0;
+	    p->velocity.x = 0;
+	    if(randy <= 5)
+		p->velocity.y = 8;
+	    if(randy > 5)
+		p->velocity.y = -8;
+	    xcount = 0;
 	}
     }
     if (p->s.center.x + p->s.width >= WINDOW_WIDTH-20 && p->velocity.x > 0) {
 	p->s.center.x = WINDOW_WIDTH - 20;
 	p->velocity.x *= -1;
 	xcount++;
-	if(xcount == 2) {
-	    if(p->velocity.x == directionx) {	
-		int randy = rand() % 10;
-		cout << "randy: " << randy << endl;
-		if(randy <= 5)
-		    p->velocity.y = 8;
-		if(randy > 5)
-		    p->velocity.y = -8;
-		xcount = 0;
-	    }
+	if(xcount >= 2) {
+	    int randy = rand() % 10;
+	    cout << "randy: " << randy << endl;
+	    if(randy <= 5)
+		p->velocity.y = 8;
+	    if(randy > 5)
+		p->velocity.y = -8;
+	    xcount = 0;
 	}
     }
     p->s.center.x += p->velocity.x;
