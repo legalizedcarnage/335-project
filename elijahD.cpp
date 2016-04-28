6eeed57f1a5df74a42c89c7dc097f0ddf08fda0c
Elijah Davis <nashisback96@gmail.com>
2016-04-28 10:19:29 -0700

    Added files via upload

--- elijahD.cpp
+++ elijahD.cpp
@@ -31,26 +31,30 @@ void playerCollision(Game *game)
 	p = &game->player;
 	//floor
 	if (p->s.center.y - p->s.height == 0 && p->velocity.y < 0) {
-		p->s.center.y = p->s.height;
-		p->velocity.y = 0;
+		p->s.center.y = WINDOW_HEIGHT - p->s.height;
+		//p->s.center.y = p->s.height;
+		//p->velocity.y = 0;
 		shiftScreen(game, 'd');
 	}
 	//roof
 	if (p->s.center.y + p->s.height == WINDOW_HEIGHT && p->velocity.y > 0) {
-		p->s.center.y = WINDOW_HEIGHT - p->s.height;
-		p->velocity.y = 0;
+		//p->s.center.y = WINDOW_HEIGHT - p->s.height;
+		p->s.center.y = p->s.height;
+		//p->velocity.y = 0;
 		shiftScreen(game, 'u');
 	}
 	//left wall
 	if (p->s.center.x - p->s.width <= 0 && p->velocity.x < 0) {
-		p->s.center.x = p->s.width;
-		p->velocity.x = 0;
+		p->s.center.x = WINDOW_WIDTH - p->s.width;
+		//p->s.center.x = p->s.width;
+		//p->velocity.x = 0;
 		shiftScreen(game, 'l');
 	}
 	//right wall
 	if (p->s.center.x + p->s.width >= WINDOW_WIDTH && p->velocity.x > 0) {
-		p->s.center.x = WINDOW_WIDTH - p->s.width;
-		p->velocity.x = 0;
+		p->s.center.x = p->s.width;
+		//p->s.center.x = WINDOW_WIDTH - p->s.width;
+		//p->velocity.x = 0;
 		shiftScreen(game, 'r');
 	}
 	
