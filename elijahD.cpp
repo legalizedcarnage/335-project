f2e11a02b3dad7f0fe1d4cd762e29f28f66446f7
Elijah Davis <nashisback96@gmail.com>
2016-04-28 11:34:13 -0700

    Added files via upload

--- elijahD.cpp
+++ elijahD.cpp
@@ -1,3 +1,6 @@
+//Elijah Davis
+//335
+//screen transitions and collisions
 #include <iostream>
 #include "main.h"
 using namespace std;
@@ -20,15 +23,36 @@ void shiftScreen(Game *game, char direction)
 }
 void playerCollision(Game *game)
 {
-	//detect object collisions
-	/*Shape *s;
-	for (int i; i < game->num_objects; i++) {
-		s = &game->object[i];
-		
-	}*/
-	//detect screen collisions
 	Player *p;
 	p = &game->player;
+
+	//detect object collisions (BAD! FIX IT!)
+	Shape *s;
+	for (int i = 0; i < game->num_objects; i++) {
+		s = &game->object[i];
+		if (p->s.center.y + p->s.height >= s->center.y- s->height
+		&& p->s.center.y - p->s.height <= s->center.y + s->height
+		&& p->s.center.x  + p->s.width >= s->center.x - s->width
+		&& p->s.center.x - p->s.width <= s->center.x + s->width) {
+			if (p->velocity.y > 0) {
+				p->s.center.y -= p->velocity.y;
+				p->velocity.y = 0;
+			}
+			if (p->velocity.y < 0) {
+				p->s.center.y += p->velocity.y;
+				p->velocity.y = 0;
+			}
+			if (p->velocity.x < 0) {
+				p->s.center.x -= p->velocity.x;
+				p->velocity.x = 0;
+			}
+			if (p->velocity.x > 0) {
+				p->s.center.x += p->velocity.x;
+				p->velocity.x = 0;
+			}
+		}		
+	}
+	//detect screen collisions
 	//floor
 	if (p->s.center.y - p->s.height == 0 && p->velocity.y < 0) {
 		p->s.center.y = WINDOW_HEIGHT - p->s.height;
