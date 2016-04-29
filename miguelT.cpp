cb8f2efbd9b2e6f4148188b61ac2df7f6f8a3e02
Miguel Trigueros <mtrigueros1@csub.edu>
2016-04-29 12:27:37 -0700

    Added files via upload

--- miguelT.cpp
+++ miguelT.cpp
@@ -1,38 +1,46 @@
+//Author: Miguel Trigueros
+//Purpose: I will be creating the enemies and allowing them to move and hunt the player.
+//Progress: Currently have basic movement but I need to make an enemy stay within its own tile.
 #include <iostream>
 #include "main.h"
 #include <cstdlib>
+#include <GL/glx.h>
+
 using namespace std;
-int xcount;
-int ycount;
+
+int xcount[10][1];
+int ycount[10][1];
 int directionx;
 int directiony;
 
 void initEnemies(Game *game) 
 {
-    game->enemies[0].s.width = 20;
-    game->enemies[0].s.height = 30;
-    game->enemies[0].velocity.x = 8;
-    game->enemies[0].velocity.y = 0;
-    game->enemies[0].s.center.x = 120 + 5*65;
-    game->enemies[0].s.center.y = 500 - 5*60;
-    ycount = 0;
-    xcount = 0;
+    for (int i = 0; i < 10; i++) {
+	game->enemies[i].s.width = 20;
+	game->enemies[i].s.height = 30;
+	game->enemies[i].velocity.x = 8;
+	game->enemies[i].velocity.y = 0;
+	game->enemies[i].s.center.x = 120 + 5*65;
+	game->enemies[i].s.center.y = 500 - 5*60;
+	ycount[i][0] = 0;
+	xcount[i][0] = 0;
+    }
 }
 
-void enemiesMovement(Game *game) 
+void enemiesMovement(Game *game, int i) 
 {
     Player *p;
-    p = &game->enemies[0];
-    if(xcount == 0)
+    p = &game->enemies[i];
+    if(xcount[i][0] == 0)
 	directionx = p->velocity.x;
-    if(ycount == 0)
+    if(ycount[i][0] == 0)
 	directiony = p->velocity.y;
 
     if (p->s.center.y - p->s.height <= 20 && p->velocity.y < 0) {
 	p->s.center.y = p->s.height + 20;
 	p->velocity.y *= -1;
-	ycount++;
-	if(ycount >= 2) {
+	ycount[i][0] += 1;
+	if(ycount[i][0] >= 2) {
 	    int randx = rand() % 10;
 	    cout << "randx: " << randx << endl;
 	    p->velocity.x = 0;
@@ -41,14 +49,14 @@ void enemiesMovement(Game *game)
 		p->velocity.x = 8;
 	    if(randx > 5)
 		p->velocity.x = -8;
-	    ycount = 0;
+	    ycount[i][0] = 0;
 	}
     }
     if (p->s.center.y + p->s.height >= WINDOW_HEIGHT-20 && p->velocity.y > 0) {
 	p->s.center.y = WINDOW_HEIGHT - p->s.height - 20;
 	p->velocity.y *= -1;
-	ycount++;
-	if(ycount >= 2) {
+	ycount[i][0] += 1;
+	if(ycount[i][0] >= 2) {
 	    int randx = rand() % 10;
 	    cout << "randx: " << randx << endl;
 	    p->velocity.x = 0;
@@ -57,14 +65,14 @@ void enemiesMovement(Game *game)
 		p->velocity.x = 8;
 	    if(randx > 5)
 		p->velocity.x = -8;
-	    ycount = 0;
+	    ycount[i][0] = 0;
 	}
     }
     if (p->s.center.x - p->s.width <= 20 && p->velocity.x < 0) {
 	p->s.center.x = p->s.width + 20;
 	p->velocity.x *= -1;
-	xcount++;
-	if(xcount >= 2) {
+	xcount[i][0] += 1;
+	if(xcount[i][0] >= 2) {
 	    int randy = rand() % 10;
 	    cout << "randy: " << randy << endl;
 	    p->velocity.x = 0;
@@ -73,25 +81,47 @@ void enemiesMovement(Game *game)
 		p->velocity.y = 8;
 	    if(randy > 5)
 		p->velocity.y = -8;
-	    xcount = 0;
+	    xcount[i][0] = 0;
 	}
     }
     if (p->s.center.x + p->s.width >= WINDOW_WIDTH-20 && p->velocity.x > 0) {
 	p->s.center.x = WINDOW_WIDTH - 20;
 	p->velocity.x *= -1;
-	xcount++;
-	if(xcount >= 2) {
+	xcount[i][0] += 1;
+	if(xcount[i][0] >= 2) {
 	    int randy = rand() % 10;
 	    cout << "randy: " << randy << endl;
 	    if(randy <= 5)
 		p->velocity.y = 8;
 	    if(randy > 5)
 		p->velocity.y = -8;
-	    xcount = 0;
+	    xcount[i][0] = 0;
 	}
     }
     p->s.center.x += p->velocity.x;
     p->s.center.y += p->velocity.y;
     cout << "x: " << p->velocity.x << endl;
     cout << "y: " << p->velocity.y << endl;
+}
+
+void renderEnemies(Game *game)
+{
+    for (int i = 0; i < 10; i++) {
+	enemiesMovement(game, i);
+	float h, w;
+	Shape *s;
+	glColor3ub(250,50,50);
+	s = &game->enemies[i].s;
+	glPushMatrix();
+	glTranslatef(s->center.x, s->center.y, s->center.z);
+	w = s->width;
+	h = s->height;
+	glBegin(GL_QUADS);
+	glVertex2i(-w,-h);
+	glVertex2i(-w, h);
+	glVertex2i( w, h);
+	glVertex2i( w,-h);
+	glEnd();
+	glPopMatrix();
+    }
 }
--- miguelT.h
+++ miguelT.h
@@ -1,5 +1,6 @@
 #ifndef MIGUEL_T
 #define MIGUEL_T
 void initEnemies(Game *game);
-void enemiesMovement(Game *game);
+void enemiesMovement(Game *game, int i);
+void renderEnemies(Game *game);
 #endif
