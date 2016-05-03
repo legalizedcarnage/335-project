a932d85570b64df43334b8a657b2c5e202c2a695
Miguel Trigueros <mtrigueros1@csub.edu>
2016-05-03 16:08:17 -0700

    Added files via upload

--- miguelT.cpp
+++ miguelT.cpp
@@ -10,7 +10,8 @@
 #include <GL/glx.h>
 
 using namespace std;
-
+// Using these counts to keep track of an emenies moves
+// This is so it doesnt get stuck doing the same thing.
 int xcount[10][1];
 int ycount[10][1];
 int directionx;
@@ -21,14 +22,14 @@ void initEnemies(Game *game)
     //Currently initializing 5 enemies for testing
     //will become dynamic when I apply difficulty per level
     for (int i = 0; i < 5; i++) {
-	game->enemies[i].s.width = 20;
-	game->enemies[i].s.height = 30;
-	game->enemies[i].velocity.x = 4;
-	game->enemies[i].velocity.y = 0;
-	game->enemies[i].s.center.x = 120 + 5*65;
-	game->enemies[i].s.center.y = 500 - 5*60;
-	ycount[i][0] = 0;
-	xcount[i][0] = 0;
+        game->enemies[i].s.width = 20;
+        game->enemies[i].s.height = 30;
+        game->enemies[i].velocity.x = 4;
+        game->enemies[i].velocity.y = 0;
+        game->enemies[i].s.center.x = 120 + 5*65;
+        game->enemies[i].s.center.y = 500 - 5*60;
+        ycount[i][0] = 0;
+        xcount[i][0] = 0;
     }
 }
 
@@ -36,73 +37,75 @@ void enemiesMovement(Game *game, int i)
 {
     Player *p;
     p = &game->enemies[i];
-    if(xcount[i][0] == 0)
-	directionx = p->velocity.x;
-    if(ycount[i][0] == 0)
-	directiony = p->velocity.y;
+
+    if (xcount[i][0] == 0)
+        directionx = p->velocity.x;
+    if (ycount[i][0] == 0)
+        directiony = p->velocity.y;
+
     //Checks for collision with walls
     //still need collision with objects
     if (p->s.center.y - p->s.height <= 20 && p->velocity.y < 0) {
-	p->s.center.y = p->s.height + 20;
-	p->velocity.y *= -1;
-	ycount[i][0] += 1;
-	if(ycount[i][0] >= 2) {
-	    int randx = rand() % 10;
-	    cout << "randx: " << randx << endl;
-	    p->velocity.x = 0;
-	    p->velocity.y = 0;
-	    if(randx <= 5)
-		p->velocity.x = 4;
-	    if(randx > 5)
-		p->velocity.x = -4;
-	    ycount[i][0] = 0;
-	}
+        p->s.center.y = p->s.height + 20;
+        p->velocity.y *= -1;
+        ycount[i][0] += 1;
+        if (ycount[i][0] >= 2) {
+            int randx = rand() % 10;
+            cout << "randx: " << randx << endl;
+            p->velocity.x = 0;
+            p->velocity.y = 0;
+            if (randx <= 5)
+                p->velocity.x = 4;
+            if (randx > 5)
+                p->velocity.x = -4;
+            ycount[i][0] = 0;
+        }
     }
     if (p->s.center.y + p->s.height >= WINDOW_HEIGHT-20 && p->velocity.y > 0) {
-	p->s.center.y = WINDOW_HEIGHT - p->s.height - 20;
-	p->velocity.y *= -1;
-	ycount[i][0] += 1;
-	if(ycount[i][0] >= 2) {
-	    int randx = rand() % 10;
-	    cout << "randx: " << randx << endl;
-	    p->velocity.x = 0;
-	    p->velocity.y = 0;
-	    if(randx <= 5)
-		p->velocity.x = 4;
-	    if(randx > 5)
-		p->velocity.x = -4;
-	    ycount[i][0] = 0;
-	}
+        p->s.center.y = WINDOW_HEIGHT - p->s.height - 20;
+        p->velocity.y *= -1;
+        ycount[i][0] += 1;
+        if (ycount[i][0] >= 2) {
+            int randx = rand() % 10;
+            cout << "randx: " << randx << endl;
+            p->velocity.x = 0;
+            p->velocity.y = 0;
+            if (randx <= 5)
+                p->velocity.x = 4;
+            if (randx > 5)
+                p->velocity.x = -4;
+            ycount[i][0] = 0;
+        }
     }
     if (p->s.center.x - p->s.width <= 20 && p->velocity.x < 0) {
-	p->s.center.x = p->s.width + 20;
-	p->velocity.x *= -1;
-	xcount[i][0] += 1;
-	if(xcount[i][0] >= 2) {
-	    int randy = rand() % 10;
-	    cout << "randy: " << randy << endl;
-	    p->velocity.x = 0;
-	    p->velocity.x = 0;
-	    if(randy <= 5)
-		p->velocity.y = 4;
-	    if(randy > 5)
-		p->velocity.y = -4;
-	    xcount[i][0] = 0;
-	}
+        p->s.center.x = p->s.width + 20;
+        p->velocity.x *= -1;
+        xcount[i][0] += 1;
+        if (xcount[i][0] >= 2) {
+            int randy = rand() % 10;
+            cout << "randy: " << randy << endl;
+            p->velocity.x = 0;
+            p->velocity.x = 0;
+            if (randy <= 5)
+                p->velocity.y = 4;
+            if (randy > 5)
+                p->velocity.y = -4;
+            xcount[i][0] = 0;
+        }
     }
     if (p->s.center.x + p->s.width >= WINDOW_WIDTH-20 && p->velocity.x > 0) {
-	p->s.center.x = WINDOW_WIDTH - 20;
-	p->velocity.x *= -1;
-	xcount[i][0] += 1;
-	if(xcount[i][0] >= 2) {
-	    int randy = rand() % 10;
-	    cout << "randy: " << randy << endl;
-	    if(randy <= 5)
-		p->velocity.y = 4;
-	    if(randy > 5)
-		p->velocity.y = -4;
-	    xcount[i][0] = 0;
-	}
+        p->s.center.x = WINDOW_WIDTH - 20;
+        p->velocity.x *= -1;
+        xcount[i][0] += 1;
+        if (xcount[i][0] >= 2) {
+            int randy = rand() % 10;
+            cout << "randy: " << randy << endl;
+            if (randy <= 5)
+                p->velocity.y = 4;
+            if (randy > 5)
+                p->velocity.y = -4;
+            xcount[i][0] = 0;
+        }
     }
     p->s.center.x += p->velocity.x;
     p->s.center.y += p->velocity.y;
@@ -120,21 +123,21 @@ void playerFound(Game *game, int i)
     //Checks distance between current enemy and player
     //still need to improve how the enemy acts when its within threshold
     if ( sqrt((pow(e->s.center.x - p->s.center.x, 2)) + (pow(e->s.center.y - p->s.center.y, 2))) <= 150) {
-	cout << "Player Found!" << endl;
-	if (p->s.center.x < e->s.center.x && e->velocity.x > 0) {
-	    e->velocity.x *= -1;
-	}
-	if (p->s.center.x > e->s.center.x && e->velocity.x < 0) {
-	    e->velocity.x *= -1;
-	}
-	if (p->s.center.y < e->s.center.y && e->velocity.y > 0) {
-	    e->velocity.y *= -1;
-	}
-	if (p->s.center.y > e->s.center.y && e->velocity.y < 0) {
-	    e->velocity.y *= -1;
-	}
-	//e->velocity.x = p->velocity.x;
-	//e->velocity.y = p->velocity.y;
+        cout << "Player Found!" << endl;
+        if (p->s.center.x < e->s.center.x && e->velocity.x > 0) {
+            e->velocity.x *= -1;
+        }
+        if (p->s.center.x > e->s.center.x && e->velocity.x < 0) {
+            e->velocity.x *= -1;
+        }
+        if (p->s.center.y < e->s.center.y && e->velocity.y > 0) {
+            e->velocity.y *= -1;
+        }
+        if (p->s.center.y > e->s.center.y && e->velocity.y < 0) {
+            e->velocity.y *= -1;
+        }
+        //e->velocity.x = p->velocity.x;
+        //e->velocity.y = p->velocity.y;
     }
     e->s.center.x += e->velocity.x;
     e->s.center.y += e->velocity.y;
@@ -145,23 +148,23 @@ void playerFound(Game *game, int i)
 void renderEnemies(Game *game)
 {
     for (int i = 0; i < 5; i++) {
-	enemiesMovement(game, i);
-	playerFound(game, i);
-	float h, w;
-	Shape *s;
-	glColor3ub(250,50,50);
-	s = &game->enemies[i].s;
-	glPushMatrix();
-	glTranslatef(s->center.x, s->center.y, s->center.z);
-	w = s->width;
-	h = s->height;
-	glBegin(GL_QUADS);
-	glVertex2i(-w,-h);
-	glVertex2i(-w, h);
-	glVertex2i( w, h);
-	glVertex2i( w,-h);
-	glEnd();
-	glPopMatrix();
+        enemiesMovement(game, i);
+        playerFound(game, i);
+        float h, w;
+        Shape *s;
+        glColor3ub(250,50,50);
+        s = &game->enemies[i].s;
+        glPushMatrix();
+        glTranslatef(s->center.x, s->center.y, s->center.z);
+        w = s->width;
+        h = s->height;
+        glBegin(GL_QUADS);
+        glVertex2i(-w,-h);
+        glVertex2i(-w, h);
+        glVertex2i( w, h);
+        glVertex2i( w,-h);
+        glEnd();
+        glPopMatrix();
     }
 }
 
