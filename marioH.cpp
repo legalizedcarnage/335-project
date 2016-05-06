78a70ea9148cf947dde20eb6cf22fd334fe81f03
mhernandez94 <mariogvhsbulldogs@yahoo.com>
2016-05-06 11:22:11 -0700

    Added files via upload

--- marioH.cpp
+++ marioH.cpp
@@ -20,8 +20,10 @@ void displayMenu(Game * game)
 	glClearColor(0.5,0.5,0.5,1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3ub(0,60,200);
-	declareobject(game,1,150,50,250+5*65,800-5*60);
-	declareobject(game,2,150,50,250+5*65,650-5*60);
+	declareobject(game,1,150,50,250+5*65,800-5*60);//play button
+	declareobject(game,2,150,50,250+5*65,650-5*60);//quit button
+	glColor3ub(100,0,0);
+	//declareobject(game,3,150,20,100,100);
 	drawobject(game,1);
 	drawobject(game,2);
 	Rect pButton;
@@ -51,6 +53,7 @@ int mainMenuCursor(XEvent *e,Game * game)
 	if (game->state ==0) {
 		if (e->type == KeyPress) {
 			int key = XLookupKeysym(&e->xkey, 0);
+			cout << key << endl;
 			if (key == XK_Escape) {
 				return 1;
 			
@@ -69,7 +72,7 @@ int mainMenuCursor(XEvent *e,Game * game)
 				
 				}
 			}
-			if (key == XK_space || "\n") {
+			if (key == XK_space || key == 65293) {
 				switch(cursorPos) {
 					case 0:
 					game->state = 1; //play button case
