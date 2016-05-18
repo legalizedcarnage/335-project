7db37bb7d116de7b3dd6f3fddbdd9c1bc5351607
Julia Acosta <jacosta16@csub.edu>
2016-05-18 12:47:57 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -192,20 +192,17 @@ void init_opengl(void)
     
     //background
     glBindTexture(GL_TEXTURE_2D, bgTexture);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, 3,
 		bgImage->width, bgImage->height,
 		0, GL_RGB, GL_UNSIGNED_BYTE, bgImage->data);
-	//background transparent part
-	//
-/*	glBindTexture(GL_TEXTURE_2D, bgTransTexture);
-	//
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-	//
-	//must build a new set of data...
-	int w = bgTransImage->width;
+    //background transparent part
+/*    glBindTexture(GL_TEXTURE_2D, bgTransTexture);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    //must build a new set of data...
+    	int w = bgTransImage->width;
 	int h = bgTransImage->height;
 	unsigned char *ftData = buildAlphaData(bgTransImage);	
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0,
