5512b5f459191bbcb64c9aa631cb281943cd3fa2
mhernandez94 <mariogvhsbulldogs@yahoo.com>
2016-05-24 17:17:11 -0700

    Add files via upload
    health bar depletes and resets

--- marioH.cpp
+++ marioH.cpp
@@ -16,6 +16,7 @@
 #include <GL/glx.h>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+#include <string>
 extern "C" {
 #include "fonts.h"
 }
@@ -54,13 +55,13 @@ void displayMenu(Game * game)
 	//glClearColor(0.5,0.5,0.5,1.0);
 	//glClear(GL_COLOR_BUFFER_BIT);
 	glColor3ub(0,60,200);
-	declareobject(game,1,150,50,250+5*65,800-5*60);//play button
-	declareobject(game,2,150,50,250+5*65,650-5*60);//quit button
+	declareobject(game,1,150,50,250+5*65,650-5*60);//play button
+	declareobject(game,2,150,50,250+5*65,500-5*60);//quit button
 	drawobject(game,1);
 	drawobject(game,2);
 	Rect pButton;
 	Rect qButton;
-	pButton.bot = 500;
+	pButton.bot = 350;
 	pButton.left = 520;
 	pButton.center = 0;
 	if (cursorPos ==0) {
@@ -69,7 +70,7 @@ void displayMenu(Game * game)
 	}
 	else
 	ggprint16(&pButton, 76, 0x00000000, "Play Game");
-	qButton.bot = 350;
+	qButton.bot = 200;
 	qButton.left = 520;
 	qButton.center = 0;
 	if (cursorPos ==1) {
@@ -78,19 +79,20 @@ void displayMenu(Game * game)
 	}
 	else
 	ggprint16(&qButton, 76, 0x00000000, "Quit Game");
-	glColor3ub(200,200,200);
+	glColor3ub(200,200,200); //reset to white to keep background image
 }
 
 void hudDisplay(Game * game) 
 {
-	glColor3ub(100,0,0);
-	declareobject(game,game->num_objects+1,150,10,250,860);
+	const char hT = game->player.health + '0';
+    	glColor3ub(100,0,0);
+	declareobject(game,game->num_objects+1,game->player.health*15,10,250,860);
 	drawobject(game,game->num_objects+1);
 	Rect health;
 	health.bot = 870;
 	health.left = 210;
 	health.center = 0;
-	ggprint16(&health, 76, 0x00ffffff, "Health Bar");
+	ggprint16(&health, 76, 0x00ffffff, "Health Bar: ");
 }
 
 int mainMenuCursor(XEvent *e,Game * game)
