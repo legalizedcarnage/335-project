eee01cc08659d6408e7b4466b0784f790494ac21
Julia Acosta <jacosta16@csub.edu>
2016-05-25 13:00:11 -0700

    Update juliaA.cpp

--- juliaA.cpp
+++ juliaA.cpp
@@ -89,9 +89,9 @@ void winner(Game *game)
 	glTexCoord2f(1.0f, 1.0f); glVertex2i(WINDOW_WIDTH, 0);
 	glEnd();
 
-	usleep(10000);
+	//usleep(3000000);
 
-	game->state = 0;
+	//game->state = 0;
 }
 void printtile(Game *game) 
 {
@@ -109,28 +109,6 @@ void printtile(Game *game)
 	declareobject(game, 5, 600, 12, 600, 8);
 	declareobject(game, 6, 12, 350, 1192, 600);
 	
-	//jail cell gate
-/*	int gate = 1;
-	if (gate == 1) {
-		declareobject(game, 7, 12, 10, 1192, 200);
-		declareobject(game, 8, 12, 10, 1192, 150);
-		declareobject(game, 9, 12, 10, 1192, 100);
-		declareobject(game, 10, 12, 10, 1192, 50);
-	}
-	else 
-	    gate = 2;
-	//card
-	int card = 1;
-	if (card == 1) {
-		declareobject(game, 11, 10, 10, 1000, 800);
-		glColor3ub(255,0,0);
-		drawobject(game, 11);
-	}
-	if () {
-	   card = 0;	
-	   gate = 0;
-	}
-*/
 	game->num_objects = 11; //7 or 11;
 
 	//bed 1
